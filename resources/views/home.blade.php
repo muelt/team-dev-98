dd9c735c776793ed534374b1d9e1843a1ab1e4bc
mizuno9101 <mizuno9101@gmail.com>
2022-08-30 18:45:28 +0900

    ホーム画面追加

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>旅のしおり_ホーム画面</title>
+    <meta name="description" content="テキストテキストテキストテキストテキストテキストテキストテキスト">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+<body>
+    <header id="header">
+      <h1>Home</h1>
+      <nav>
+        <ul>
+          <li><a href=" ">旅先一覧</a></li>
+        </ul>
+      </nav>
+    </header>
+<main>
+
+   
