ffc9d84d253f76830cc95d022868f4fbaab0ab6d
mizuno9101 <mizuno9101@gmail.com>
2022-09-05 19:35:58 +0900

    ホーム画面修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,13 +9,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
+
+        @isset($title)
+        <title>{{ $title }} - 旅のしおり_ホーム</title>
+        @else
         <title>旅のしおり_ホーム</title>
-        <!-- Favicon-->
+        @endisset
+
+        <!-- ファビコン-->
         <link rel="icon" type="image/x-icon" href="{{ 'assets/favicon.ico' }}" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+        
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
     </head>
---
+++ resources/views/nav.blade.php
@@ -0,0 +1,7 @@
+<!-- Navigation-->
+<nav class="navbar navbar-light bg-light static-top">
+   <div class="container">
+       <a class="navbar-brand" href="{{ url('/') }}">ホーム</a>
+       <a class="btn btn-primary" href="{{ url('') }}">旅先一覧</a>
+   </div>
+</nav>
