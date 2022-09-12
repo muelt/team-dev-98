97a948171d57ea030e223ff0d734bc5328a5a0fb
mizuno9101 <mizuno9101@gmail.com>
2022-09-12 14:49:48 +0900

    AllTripsダミーテキストを日本語化

--- database/factories/TripFactory.php
+++ database/factories/TripFactory.php
@@ -18,11 +18,11 @@ class TripFactory extends Factory
     public function definition()
     {
         return [
-            'title' => $this->faker->sentence(mt_rand(3,8)),
+            'title' => $this->faker->realText(10)/*(mt_rand(3,8))*/,
             'slug' => $this->faker->slug(),
             'date' => $this->faker->dateTimeBetween('-5 weeks', '-1 week'),
             // 'cities' => $this->faker->streetName(),
-            'body' => $this->faker->sentence(mt_rand(40,60)),
+            'body' => $this->faker->realText(20)/*(mt_rand(40,60))*/,
             'category_id' => mt_rand(1,3),
             'user_id' => mt_rand(1,5),
             'prefecture_id' => mt_rand(1,4),
