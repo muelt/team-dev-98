9206c97f5a65928c7cd281971c0f581737664940
mizuno9101 <mizuno9101@gmail.com>
2022-09-20 18:48:20 +0900

    保留

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
     <!-- ユーザー名表示 -->
+    
     <a class="navbar-brand"><h1>こんにちは、<?php $user = Auth::user(); ?>{{ $user -> name }}</h1></a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
